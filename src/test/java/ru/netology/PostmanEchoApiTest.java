69c6a352beddbc07d9189a39afc7fabef53cd841
Sergey <tel.cromax@gmail.com>
2021-09-25 21:26:45 +1100

    JsonPath invalid

--- src/test/java/ru/netology/PostmanEchoApiTest.java
+++ src/test/java/ru/netology/PostmanEchoApiTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoApiTest {
             .post("/post")
         .then()
             .statusCode(200)
-            .body("data", equalTo("value"))
+            .body("nodata", equalTo("value"))
         ;
 
     }
